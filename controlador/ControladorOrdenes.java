45a5a2e1ea61e0463f245bb723c4b6bcf6af3d08
Luxrayjp <nestpihu@espol.edu.ec>
2025-06-25 20:09:02 -0500

    Create ControladorOrdenes.java

---
+++ controlador/ControladorOrdenes.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import modelo.OrdenServicio;
+import java.util.ArrayList;
+
+public class ControladorOrdenes {
+    private ArrayList<OrdenServicio> listaOrdenes;
+
+    public ControladorOrdenes() {
+        listaOrdenes = new ArrayList<>();
+    }
+
+    public void agregarOrden(OrdenServicio orden) {
+        listaOrdenes.add(orden);
+    }
+
+    public ArrayList<OrdenServicio> getListaOrdenes() {
+        return listaOrdenes;
+    }
+}
